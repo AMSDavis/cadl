fbfabb13dcd3c0deb66f936c18e44f55de7aecea
Jianye Xi <jianye.xi@microsoft.com>
2021-11-22 21:14:01 +0800

    support showing linting messag in error list of vs

--- packages/cadl-msbuild-target/CadlCompile.cs
+++ packages/cadl-msbuild-target/CadlCompile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -130,6 +131,18 @@ namespace Cadl.Tools
             return true;
         }
 
+        protected override void LogEventsFromTextOutput(string singleLine, MessageImportance messageImportance) {
+
+          foreach (CadlLogFilter filter in cadlLogFilters) {
+            Match match = filter.Pattern.Match(singleLine);
+            if (match.Success) {
+              filter.LogAction(Log, match);
+              return;
+            }
+          }
+          base.LogEventsFromTextOutput(singleLine, messageImportance);
+        }
+
         protected override string ToolName => OS.Equals("Windows_NT") ? "cadl.cmd" : "cadl";
 
 
@@ -146,5 +159,62 @@ namespace Cadl.Tools
 
         protected override bool ValidateParameters() => true;
 
+        static readonly List<CadlLogFilter> cadlLogFilters = new List<CadlLogFilter>()
+        {
+            // Example of warning filter
+            //C:\code\cadl\main.cadl:33:1 - warning @azure-tools/cadl-rpaas/model-requires-documentation: The model must have a documentation or description, please use decorator @doc to add it.
+            new CadlLogFilter
+            {
+                Pattern = new Regex(
+                    pattern: "^(?'FILENAME'.+?):(?'LINE'\\d+):(?'COLUMN'\\d+) - warning (?'CODE'.+?): ?(?'TEXT'.*)",
+                    options: RegexOptions.Compiled | RegexOptions.IgnoreCase),
+                LogAction = (log, match) =>
+                {
+                    int.TryParse(match.Groups["LINE"].Value, out var line);
+                    int.TryParse(match.Groups["COLUMN"].Value, out var column);
+
+                    log.LogWarning(
+                        subcategory: null,
+                        warningCode: match.Groups["CODE"].Value,
+                        helpKeyword: null,
+                        file: match.Groups["FILENAME"].Value,
+                        lineNumber: line,
+                        columnNumber: column,
+                        endLineNumber: 0,
+                        endColumnNumber: 0,
+                        message: match.Groups["TEXT"].Value);
+                }
+            },
+
+            // Example of error filter
+            //C:\code\cadl\main.cadl:33:1 - error @azure-tools/cadl-rpaas/missing-required-prop: Resource configuration is missing required 'parameterType' property
+            new CadlLogFilter
+            {
+                Pattern = new Regex(
+                    pattern: "^(?'FILENAME'.+?):(?'LINE'\\d+):(?'COLUMN'\\d+) - error (?'CODE'.+?): ?(?'TEXT'.*)",
+                    options: RegexOptions.Compiled | RegexOptions.IgnoreCase),
+                LogAction = (log, match) =>
+                {
+                    int.TryParse(match.Groups["LINE"].Value, out var line);
+                    int.TryParse(match.Groups["COLUMN"].Value, out var column);
+
+                    log.LogError(
+                        subcategory: null,
+                        errorCode: match.Groups["CODE"].Value,
+                        helpKeyword: null,
+                        file: match.Groups["FILENAME"].Value,
+                        lineNumber: line,
+                        columnNumber: column,
+                        endLineNumber: 0,
+                        endColumnNumber: 0,
+                        message: match.Groups["TEXT"].Value);
+                }
+            }
+        };
+
+        class CadlLogFilter {
+          public Regex Pattern { get; set; }
+          public Action<TaskLoggingHelper, Match> LogAction { get; set; }
+        }
     }
 }
